c24d94d7edfe1d03753a39c395ab211961076bb8
theexplosionmage <otar.phrodiashvili.1@btu.edu.ge>
2022-04-13 17:42:57 +0400

    Update MainActivity.kt

--- app/src/main/java/com/example/midterm/MainActivity.kt
+++ app/src/main/java/com/example/midterm/MainActivity.kt
@@ -23,24 +23,25 @@ class MainActivity : AppCompatActivity() {
     val id6: EditText = findViewById(R.id.id6)
 
     btnBuy.setOnClickListener{
+
         try {
             if(id1.text.toString().toFloat() >= 100000 || id1.text.toString().toFloat() <= 0)
                 Toast.makeText(this,"Balance out of range",Toast.LENGTH_LONG).show()
+            else if(id2.text.toString().length > 50 || id2.text.toString().length < 5)
+                Toast.makeText(this,"Invalid Name",Toast.LENGTH_LONG).show()
+            else if(id3.text.toString().length != 16)
+                Toast.makeText(this,"Invalid Credit Card Number",Toast.LENGTH_LONG).show()
+            else if(id4.text.toString().length !=2)
+                Toast.makeText(this,"Invalid Month",Toast.LENGTH_LONG).show()
+            else if(id5.text.toString().length !=2)
+                Toast.makeText(this,"Invalid Year",Toast.LENGTH_LONG).show()
+            else if(id5.text.toString().length !=3)
+                Toast.makeText(this,"Invalid CVV Code",Toast.LENGTH_LONG).show()
+            else
+                Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
         } catch (e: NumberFormatException) {
             Toast.makeText(this,"Invalid Balance Amount",Toast.LENGTH_LONG).show()
         }
-
-        if(id2.text.toString().length > 50 || id2.text.toString().length < 5)
-            Toast.makeText(this,"Invalid Name",Toast.LENGTH_LONG).show()
-        if(id3.text.toString().length != 16)
-            Toast.makeText(this,"Invalid Credit Card Number",Toast.LENGTH_LONG).show()
-        if(id4.text.toString().length !=2)
-            Toast.makeText(this,"Invalid Month",Toast.LENGTH_LONG).show()
-        if(id5.text.toString().length !=2)
-            Toast.makeText(this,"Invalid Year",Toast.LENGTH_LONG).show()
-        if(id5.text.toString().length !=3)
-            Toast.makeText(this,"Invalid CVV Code",Toast.LENGTH_LONG).show()
-        Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
     }
 
 
